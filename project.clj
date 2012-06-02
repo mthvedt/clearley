9a409571cb49b2e0a3d5ad6b4efb650457fd9cfb
Mike Thvedt <mike.thvedt@gmail.com>
2012-06-02 17:37:31 -0500

    Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clearley "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1" :scope "test"]
                  [com.stuartsierra/lazytest "2.0.0-SNAPSHOT" :scope "test"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
