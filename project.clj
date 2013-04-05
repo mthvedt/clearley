9fb945ca9cee4d74c25291708a4d634b0fb12027
Mike Thvedt <mike.thvedt@gmail.com>
2013-04-05 15:13:35 -0500

    Update lazytest for latest lein

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clearley "0.0.2.SNAPSHOT"
+(defproject clearley "0.1.2.SNAPSHOT"
   :description "Parsing for Earthlings"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1" :scope "test"]
                  [com.stuartsierra/lazytest "2.0.0-SNAPSHOT" :scope "test"]
                  [criterium "0.3.1" :scope "test"]]
+  :plugins [[eightnotrump/lein-lazytest "1.0.5"]]
   :profiles {:benchmark {:source-paths ["benchmark" "test"]
                          :resource-paths ["benchmark"]
                          :main clearley.benchmark.test}}
