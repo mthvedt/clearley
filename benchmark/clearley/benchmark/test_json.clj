b75ff2984dc3870333f7057897c41b5887129927
Mike Thvedt <mike.thvedt@gmail.com>
2013-04-10 20:35:45 -0500

    Comparisons

--- benchmark/clearley/benchmark/test_json.clj
+++ benchmark/clearley/benchmark/test_json.clj
@@ -1,7 +1,26 @@
 (ns clearley.benchmark.test-json
   (require [clojure.java.io :as io]
-           [clearley.examples.json :as json])
+           [clearley.examples.json :as json]
+           clojure.data.json
+           clj-json.core
+           cheshire.core)
   (use clearley.core clearley.benchmark.core criterium.core))
 
+(defn compariton [str subject]
+  (println "Benchmarking for comparison:" str)
+  (let [loaded-file (slurp (get-resource "small-test-formatted.json"))]
+    (bench (subject loaded-file))))
+
 (defn -main []
-  (sanity-check json/json-parser "small-test.json" "small-test-json.clj"))
+  (sanity-check json/json-parser "small-test.json" "small-test-json.clj")
+  (sanity-check json/json-parser "small-test-formatted.json" "small-test-json.clj")
+
+  ; JSON grammar is relatively complex--LR parsers should tend to do well
+  ;(bench-from-file "JSON" json/json-parser "small-test.json")
+  ; With no lookahead, each consecutive space adds O(1) ambiguity
+  (bench-from-file "Formatted JSON" json/json-parser "small-test-formatted.json")
+
+  (compariton "clojure.core/data.json" clojure.data.json/read-str)
+  (compariton "clj-json" #(clj-json.core/parse-string %))
+  (compariton "Cheshire" #(cheshire.core/parse-string %))
+  )
--- project.clj
+++ project.clj
@@ -7,11 +7,12 @@
                                   "http://stuartsierra.com/maven2",
                                   "stuartsierra-snapshots"
                                   "http://stuartsierra.com/m2snapshots"}
-                   :dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"
-                                   :scope "test"]
-                                  [org.clojure/math.numeric-tower "0.0.1"
-                                   :scope "test"]
-                                  [criterium "0.3.1" :scope "test"]]}
-             :benchmark {:source-paths ["benchmark" "test"]
+                   :dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT" ]
+                                  [org.clojure/math.numeric-tower "0.0.1" ]
+                                  [criterium "0.3.1"]]}
+             :benchmark {:dependencies [[org.clojure/data.json "0.2.2"]
+                                        [clj-json "0.5.3"]
+                                        [cheshire "5.1.0"]]
+                         :source-paths ["benchmark" "test"]
                          :resource-paths ["benchmark"]
                          :main clearley.benchmark.test}})
