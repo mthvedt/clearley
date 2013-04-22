0bd61784134b655b8ce3356e04aacfaedb40f94c
Mike Thvedt <mike.thvedt@gmail.com>
2013-04-22 00:06:17 -0500

    Checkpoint: nda-test

--- test/clearley/test/npda.clj
+++ test/clearley/test/npda.clj
@@ -18,6 +18,7 @@
   (let [n1 (-> (npda/state (ANode. 1))
           (npda/shift-state 2 2)
           (npda/shift-state 3 3)) ; stack is 3 2 1, ostream 3 . 2 1
+        ; (the dot denotes ostreams sitting in unpopped states)
         n2 (-> n1 npda/popone npda/popone) ; should be 1
         ; stack is 4 3 2 1. spinning 4 returns 5. 3 becomes 2 + 4 = 6
         ; ostream is now 5 4 3 . 2 1
@@ -34,7 +35,7 @@
     (is= (npda/stream n2b) [2 3 4 5 7])))
 
 ; This is a little complicated
-#_(deftest nda-test
+(deftest nda-test
   (let [n1 (-> (npda/state (ANode. 1))
              (npda/shift-state 1 1)
              (npda/shift-state 2 2)
@@ -55,15 +56,14 @@
         nx (npda/pop n*) ; 3 stacks
         ny (mapcat npda/pop nx) ; Still three stacks
         ; test nondeterministic reducing
-        ; put 6 on the stack and spin... yields stack-top 9
-        ; spin that... yields 9 1, 10 2 1, 12 3 2 1
-        nr (-> n* (npda/shift-state 6 6) npda/spin-state first npda/spin-state
-             first npda/spin-state)
-        ; this should yield 5, 7 1, 9 2 1
-        nr2 (mapcat npda/spin-state nr)]
-        ;nr3 [(npda/popone (first nr2))
-         ;    (-> (second nr2) npda/spin-state first npda/popone)
-          ;   (-> (nth nr2 2) npda/spin-state first npda/spin-state first npda/popone)]]
+        ; put 6 on the stack and spin... yields stack-top 10
+        ; spin again... yields 11 1, 12 2 1, 13 3 2 1
+        nr (-> n* (npda/shift-state 6 6) npda/spin-state first npda/spin-state)
+        ; 11, 13 1, 15 2 1
+        nr2 (mapcat npda/spin-state nr)
+        nr3 [(first nr2)
+             (-> (second nr2) npda/spin-state first)
+             (-> (nth nr2 2) npda/spin-state first npda/spin-state first)]]
     (is= (:value (npda/peek n*)) 5)
     (is= (count nx) 3)
     (is= (count ny) 3)
@@ -71,17 +71,11 @@
     (is= (map-> nx npda/peek :value) [2 3 4])
     (is= (map npda/stream ny) [[1 2 5] [2 3 5] [3 4 5]])
     (is= (map-> ny npda/peek :value) [1 2 3])
-    ; Test reducing
-    ; nr: stacks are 1 2 5 6, 1 2 3 5 6, 1 2 3 4 5 6
     (is= (count nr) 3)
-    ; nr2: stacks are 1 2 1, 1 2 3 2, 1 2 3 4 3
-    ; after reducing, 1 0, 1 2 1, 1 2 3 2
     (is= (count nr2) 3)
-    (is= (map-> nr npda/peek :value) [1 2 3])
-    (is= (map-> nr2 npda/peek :value) [0 1 2])))
-    ; nr3.1: after reducing once, stack is 1 0
-    ; nr3.2: stack is 1 2 1, then 1 0
-    ;(is= (map-> nr3 npda/peek :value) [1 1 1])
-    ;(is= (map npda/stream nr3) [[1 2 5 6 7 2]
-     ;                           [1 2 3 5 6 7 3 2]
-      ;                          [1 2 3 4 5 6 7 4 3 2]])))
+    (is= (map-> nr npda/peek :value) [11 12 13])
+    (is= (map-> nr2 npda/peek :value) [11 13 15])
+    (is= (map-> nr3 npda/peek :value) [11 13 16])
+    (is= (map npda/stream nr3) [[1 2 5 6 7 11 12]
+                                [1 2 3 5 6 7 11 13 14]
+                                [1 2 3 4 5 6 7 11 14 16 17]])))
