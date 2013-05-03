dfc380ef98c943c19905db2aa600318ae0a1e51f
Mike Thvedt <mike.thvedt@gmail.com>
2013-05-03 07:22:16 -0500

    Continuations

--- src/clearley/quentin.clj
+++ src/clearley/quentin.clj
@@ -13,6 +13,12 @@
 
 (declare continue-parsing item-parser-fn)
 
+(defn tramp [initial-fn]
+  (let [r (initial-fn)]
+    (if (ifn? r)
+      (recur r)
+      r)))
+
 (defn item-parser-fn* [seeds mem]
   (let [more-seeds (mapcat #(eager-advances % false) seeds)
         {item-set-items :items :as item-set} (closed-item-set more-seeds)
@@ -20,12 +26,10 @@
         shifter-fn (memoize #(shift-item-set item-set-items %))
         return-fn (memoize #(returns more-seeds %))
         continuer (continue-parsing item-set mem)]
-    (fn [{:keys [input output pos] :as stream}]
+    (fn [{:keys [input output pos] :as stream} continuation]
       (let [current-input (first input)
             shift (if (seq input) (shifter-fn current-input))
-            ;(shift-item-set (:items item-set) current-input)
             current-input (if (seq input) current-input :clearley.clr/term)
-            ;return-set (returns more-seeds current-input)]
             return-set (return-fn current-input)]
         ;(println "===")
         ;(println "Input: " current-input)
@@ -37,7 +41,12 @@
           (println "Reduce-reduce conflict: "
                    (s/separate-str " " (map item-str-follow return-set))))
         (cond shift
-              ((continue-parsing item-set mem)
+              (fn []
+                ((item-parser-fn shift mem)
+                   (update-all stream {:input rest, :output #(conj % current-input),
+                                        :pos inc})
+                  #(continuer % continuation)))
+              #_(continuer
                  ((item-parser-fn shift mem) 
                     (update-all stream {:input rest, :output #(conj % current-input),
                                         :pos inc})))
@@ -45,7 +54,8 @@
               return-set
               ; TODO don't ignore reduce-reduce
               ; TODO put rule in output.
-              (update stream :output #(conj % (first return-set)))
+              (fn []
+                (continuation (update stream :output #(conj % (first return-set)))))
 
               true (t/RE "Failure to parse at position" pos))))))
 
@@ -62,17 +72,21 @@
 (defn continue-parsing [item-set mem]
   (let [shift-advancer (memoize #(advance-item-set item-set % false))
         continue-advancer (memoize #(advance-item-set item-set % true))]
-    (fn [{:keys [input output pos] :as stream}]
+    (fn f [{:keys [input output pos] :as stream} continuation]
       (let [current-backlink (:backlink (peek output))
             shift-advance (shift-advancer current-backlink)
             continue-advance (continue-advancer current-backlink)]
         ;(println "Stack top: " (-> current-backlink :rule :raw-rule :original))
         (if (and shift-advance continue-advance)
           (println "Stack split in item set\n" (item-set-str item-set)))
-        (cond shift-advance (recur ((item-parser-fn shift-advance mem) stream))
+        (cond shift-advance
+              ;(recur ((item-parser-fn shift-advance mem) stream))
+              (fn [] ((item-parser-fn shift-advance mem) stream #(f % continuation)))
               ; Keep us on the stack, see what comes next
 
-              continue-advance ((item-parser-fn continue-advance mem) stream)
+              continue-advance
+              #((item-parser-fn continue-advance mem) stream continuation)
+              ;((item-parser-fn continue-advance mem) stream)
               ; Basically a tailcall
 
               true (t/RE "Failure to parse at position" pos))))))
@@ -80,7 +94,9 @@
 (defn parse [grammar goal input mem mem2]
   (try
     (binding [rules/*mem-atom* mem2]
-      ((item-parser-fn [(goal-item goal grammar)] mem) (parse-stream input)))
+      (tramp #((item-parser-fn [(goal-item goal grammar)] mem)
+                 (parse-stream input)
+                 identity)))
     (catch RuntimeException e nil))) ; TODO this is a hack
 
 (defn pprint-parse-stream [{:keys [input output] :as stream}]
